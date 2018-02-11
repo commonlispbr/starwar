b2bcc90f4bd84595a2ba02d166d03490718484be
Manoel Vilela <manoel_vilela@engineer.com>
2018-02-11 04:49:45 -0300

    Simplify make-binary function

--- src/make-binary.lisp
+++ src/make-binary.lisp
@@ -1,14 +1,10 @@
 ;; using this to make linux binary. Before that, we have to configure asdf
 ;; correctly, so that ASD files can be found.
 
-#+win32
-(setq asdf:*central-registry*
-      (append asdf:*central-registry*
-	      '("Z:\\home\\xzpeter\\git-repo\\starwar\\"
-		"Z:\\home\\xzpeter\\git-repo\\starwar\\lib\\")))
+(in-package :starwar)
 
-(require "starwar")
-(sb-ext:save-lisp-and-die #+unix "starwar-linux"
-			  #+win32 "starwar-win32.exe"
-			  :toplevel #'org.xzpeter.game.starwar:main
-			  :executable t)
+(defun make-binary ()
+  (sb-ext:save-lisp-and-die #+unix "starwar-linux"
+                            #+win32 "starwar-win32.exe"
+                            :toplevel #'starwar:main
+                            :executable t))
